4f1bfad15488623ad12c6112c9eceabb523e5d99
Bernie <bernert.justin@gmail.com>
2020-08-20 17:34:25 +0200

    [fix][sidenav] une side nav en mobile un peu mieux

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ui';
+  isMobile = false;
+
+  constructor(mobileBreakPointObserver: BreakpointObserver) {
+   const layoutChange =  mobileBreakPointObserver.observe('(max-width: 768px)');
+
+   layoutChange.subscribe((result) => {
+     this.isMobile = result.matches;
+     console.log(this.isMobile);
+   });
+  }
 }
